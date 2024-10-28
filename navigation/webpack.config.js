5f35a914123d7f320c16b1ab7b784fb8a92ebcd5
Bugra Unaltay <bugraunaltay@gmail.com>
2024-10-29 00:07:11 +0300

    Configure Module Federation for navigation and shopping cart

--- navigation/webpack.config.js
+++ navigation/webpack.config.js
@@ -1,400 +1,73 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-
-
-
-
-
-
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-
-
-
-
-
-
-
-const path = require('path');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = {
-
-
-
-
-
-
-
-  entry: './src/index',
-
-
-
-
-
-
-
-  mode: 'development',
-
-
-
-
-
-
-
-  devServer: {
-
-
-
-
-
-
-
-    port: 3000,
-
-
-
-
-
-
-
-  },
-
-
-
-
-
-
-
-  output: {
-
-
-
-
-
-
-
-    publicPath: 'http://localhost:3000/',
-
-
-
-
-
-
-
-  },
-
-
-
-
-
-
-
-  module: {
-
-
-
-
-
-
-
-    rules: [
-
-
-
-
-
-
-
-      {
-
-
-
-
-
-
-
-        test: /\.(js|jsx|tsx|ts)$/,
-
-
-
-
-
-
-
-        loader: 'ts-loader',
-
-
-
-
-
-
-
-        exclude: /node_modules/,
-
-
-
-
-
-
-
-      },
-
-
-
-
-
-
-
-    ],
-
-
-
-
-
-
-
-  },
-
-
-
-
-
-
-
-  plugins: [
-
-
-
-
-
-
-
-    new ModuleFederationPlugin({
-
-
-
-
-
-
-
-      name: 'navigation',
-
-
-
-
-
-
-
-      filename: 'remoteEntry.js',
-
-
-
-
-
-
-
-      remotes: {
-
-
-
-
-
-
-
-        'product-listings': 'product_listings@http://localhost:3001/remoteEntry.js',
-
-
-
-
-
-
-
-        'shopping-cart': 'shopping_cart@http://localhost:3002/remoteEntry.js',
-
-
-
-
-
-
-
-        'user-authentication': 'user_authentication@http://localhost:3003/remoteEntry.js',
-
-
-
-
-
-
-
-        'order-history': 'order_history@http://localhost:3004/remoteEntry.js',
-
-
-
-
-
-
-
-      },
-
-
-
-
-
-
-
-      shared: ['react', 'react-dom', 'react-router-dom', '@reduxjs/toolkit', 'react-redux'],
-
-
-
-
-
-
-
-    }),
-
-
-
-
-
-
-
-    new HtmlWebpackPlugin({
-
-
-
-
-
-
-
-      template: './public/index.html',
-
-
-
-
-
-
-
-    }),
-
-
-
-
-
-
-
-  ],
-
-
-
-
-
-
-
-  resolve: {
-
-
-
-
-
-
-
-    extensions: ['.ts', '.tsx', '.js'],
-
-
-
-
-
-
-
-    alias: {
-
-
-
-
-
-
-
-      'product-listings': path.resolve(__dirname, '../product-listings/src'),
-
-
-
-
-
-
-
-      'shopping-cart': path.resolve(__dirname, '../shopping-cart/src'),
-
-
-
-
-
-
-
-      'user-authentication': path.resolve(__dirname, '../user-authentication/src'),
-
-
-
-
-
-
-
-      'order-history': path.resolve(__dirname, '../order-history/src'),
-
-
-
-
-
-
-
-      'common': path.resolve(__dirname, '../common'),
-
-
-
-
-
-
-
-    },
-
-
-
-
-
-
-
-  },
-
-
-
-
-
-
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const path = require('path');
+const deps = require('./package.json').dependencies;
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3005,
+    historyApiFallback: true,
+    hot: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  },
+  output: {
+    publicPath: 'http://localhost:3005/',
+    filename: '[name].[contenthash].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'navigation',
+      filename: 'remoteEntry.js',
+      remotes: {
+        'product-listings': 'product_listings@http://localhost:3001/remoteEntry.js',
+        'shopping-cart': 'shopping_cart@http://localhost:3002/remoteEntry.js',
+        'user-authentication': 'user_authentication@http://localhost:3003/remoteEntry.js',
+        'order-history': 'order_history@http://localhost:3004/remoteEntry.js'
+      },
+      exposes: {
+        './Navigation': './src/Navigation',
+        './ErrorBoundary': './src/ErrorBoundary'
+      },
+      shared: {
+        ...deps,
+        react: { singleton: true, eager: true, requiredVersion: deps.react },
+        'react-dom': { singleton: true, eager: true, requiredVersion: deps['react-dom'] },
+        '@reduxjs/toolkit': { singleton: true, eager: true, requiredVersion: deps['@reduxjs/toolkit'] },
+        'react-redux': { singleton: true, eager: true, requiredVersion: deps['react-redux'] },
+        'react-router-dom': { singleton: true, eager: true, requiredVersion: deps['react-router-dom'] },
+        'styled-components': { singleton: true, eager: true, requiredVersion: deps['styled-components'] }
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  optimization: {
+    moduleIds: 'named',
+  }
+};
+
+
